624cedb19478de063b40487f160840629d35c7ce
joseotavio99 <joseotavioetec@gmail.com>
2023-08-11 13:37:17 +0000

    save1

---
+++ buscador/src/App.js
@@ -0,0 +1,19 @@
+
+import { FiSearch } from "react-icons/fi";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title"> Buscador de CEP</h1>
+      <div className="containerInput">
+        <input type="text" placeholder="Digite aqui ..."/>
+        <button className="buttonSearch">
+         <FiSearch size={25} color="#FFF"/>
+         
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
